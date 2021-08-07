f0ed433c04adb64282d612bbb09529e0007ea6e9
YungChen03 <pete760221@gmail.com>
2021-08-08 01:19:38 +0800

    first finish
    first finish

--- assets/js/togglemenu.js
+++ assets/js/togglemenu.js
@@ -1,6 +1,8 @@
 const menu = document.querySelector('.menu')
+const img = document.querySelector('.menu img')
 const hide_menu = document.querySelector('.links')
 
 menu.addEventListener('click', () => {
   hide_menu.classList.toggle('active')
+  img.classList.toggle('rotate')
 })
