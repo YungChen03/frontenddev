19f8a2ece3a11aa5a84dae645c28333b7bde2e15
YungChen03 <pete760221@gmail.com>
2021-08-08 14:01:11 +0800

    add animation

--- assets/js/highlight.js
+++ assets/js/highlight.js
@@ -1,22 +1,23 @@
-const sections = document.querySelectorAll('section');
-const navLi = document.querySelectorAll('.nav a');
+const sections = document.querySelectorAll('section')
+const navLi = document.querySelectorAll('.nav-list a')
 
 window.addEventListener('scroll', () => {
-    let current = '';
+  let current = ''
 
-    sections.forEach(section => {
-        const sectionTop = section.offsetTop;
-        const sectionHeight = section.clientHeight;
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.clientHeight
 
-        if (pageYOffset >= sectionTop - sectionHeight / 3) {
-            current = section.getAttribute('id')
-        }
-    })
+    if (pageYOffset >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute('id')
+      console.log(current)
+    }
+  })
 
-    navLi.forEach(a => {
-        a.classList.remove('active');
-        if (a.classList.contains(current)) {
-            a.classList.add('active')
-        }
-    })
+  navLi.forEach((a) => {
+    a.classList.remove('active')
+    if (a.classList.contains(current)) {
+      a.classList.add('active')
+    }
+  })
 })
